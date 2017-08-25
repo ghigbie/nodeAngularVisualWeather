03f37be2867f18976b64d58625f688403d50be66
George Higbie <gehigbie@gmail.com>
2017-08-25 15:00:24 +0000

    Add request and api address to app

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-let getUser = (id, callback) => {
-    let user = {
-        id: id,
-        name: 'Vikram'
-    };
-    callback(user);
-};
+const request = require('express');
 
-getUser(31, (userObject) => {
-    console.log(userObject);
-});
+let apiAddress = `https://maps.googleapis.com/maps/api/geocode/json?address=`;
+let address = `9601%20S%20Meridian%20Blvd,%20Englewood,%20CO%2080112`;
+let inputAddress = `${apiAddress}${address}`;
+
+//request takes two arguments, the first is an options object and the second is a callback which is executed after the api call
+request(
+        { 
+    url: inputAddress,
+    json: true //this property converts json string into json object
+    }, 
+    
+    (error, response, body) => {
+        console.log(body);
+})
