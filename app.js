75c5a2a322ee955ecdee1c5426c756a1274c5ee2
George Higbie <gehigbie@gmail.com>
2017-08-26 21:30:12 +0000

    Refactor code

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const yargs = require('yargs');
 
 const geocode = require('./geocode/geocode');
+const darkSky = require ('./darkSky/darkSky');
 const apiKey = require('./apiKey');
+
+const darkAPI = `https://api.darksky.net/forecast`;
+let latitude = ``;
+let longitude = ``;
+let fullAPIRequest = ``;
 
 const argv = yargs
     .options({
@@ -21,12 +27,12 @@ geocode.geocodeAddress(argv.address, (errorMessage, results) => {
         console.log(errorMessage);
     }else{
         console.log(JSON.stringify(results, undefined, 2));
+        latitude = results.latitude;
+        longitude = results.longitude;
+        let fullAPIRequest = `${darkAPI}/${apiKey.apiKey}/${latitude},${longitude}`;
+        console.log(fullAPIRequest);
     }
 });
 
-const darkAPI = `https://api.darksky.net/forecast`;
-
-let latitude = ``;
-let longitude = ``;
-let fullAPIRequest = `${darkAPI}/${apiKey.apiKey}/${latitude},${longitude}`;
 console.log(fullAPIRequest);
+darkSky.callWeather('https://api.darksky.net/forecast/9f6325a874ba4e46242d3e5e3c349a27/39.5835785,-104.8571368');
