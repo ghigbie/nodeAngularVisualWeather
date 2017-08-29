4eac1ac17cb508ded69c6ef59d4614072a41ff82
George Higbie <gehigbie@gmail.com>
2017-08-29 16:21:31 +0000

    Install axios

---
+++ app-promise.js
@@ -0,0 +1,21 @@
+const yargs = require('yargs');
+const axios = require('axios');
+
+const argv = yargs
+    .options({
+        a:{
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+let encodedAddress = encodeURIComponent(argv.address);
+console.log(encodedAddress);
+// let geocodeAddress = (encodeAddress) => {
+    
+// }
